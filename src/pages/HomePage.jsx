856cce96b9a12bc7c523e4112cfbad1cbcb752d3
shehap ghazal <sh.s.ghazal2003@gmail.com>
2025-06-23 18:31:00 +0300

    Paymet Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -68,7 +68,7 @@ const HomePage = () => {
             <svg className="w-5 h-5 ml-2 transition-transform transform group-hover:translate-x-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
           </button>
         </div>
-        <PaymentIcons />
+        {/* <PaymentIcons /> */}
       </section>
 
       {/* FAQ Section */}
